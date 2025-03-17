05fadc3a27bee8b9e0ffe1f39638b4b09bdd6603
VivianSTWu <vivian.stwu@gmail.com>
2025-03-17 05:18:22 -0300

    Calendario personalizado

--- MeuCalendario/app/calendario.tsx
+++ MeuCalendario/app/calendario.tsx
@@ -1,46 +1,60 @@
-import { CalendarCogIcon } from "lucide-react-native";
-import React, { useState } from "react";
-import { View, Text } from "react-native";
+import React from "react";
+import { View, ScrollView } from "react-native";
 import { Calendar } from "react-native-calendars";
 
 const Calendario = () => {
-  // Simulando alguns eventos
+
   const eventos = [
     { date: "2025-03-18", title: "Consulta Dentária" },
     { date: "2025-03-20", title: "Consulta de Rotina" },
   ];
 
-  // Função para gerar as datas com eventos
+  // Obtém a data atual no formato YYYY-MM-DD
+  const today = new Date().toISOString().split("T")[0];
+
   const getMarkedDates = () => {
     let markedDates = {};
+
     eventos.forEach((evento) => {
       markedDates[evento.date] = {
-        marked: true,
-        dotColor: "red", // cor do ponto para o evento
-        activeOpacity: 0,
+        customStyles: {
+          container: {
+            borderWidth: 1,
+            borderColor: "orange", 
+            borderRadius: 10, 
+          },
+          text: {
+            color: "black",
+          },
+        },
       };
     });
+
+    markedDates[today] = {
+      customStyles: {
+        container: {
+          backgroundColor: "blue",
+          borderRadius: 10,
+        },
+        text: {
+          color: "white",
+        },
+      },
+    };
+
     return markedDates;
   };
 
   return (
-    <View style={{ flex: 1, padding: 20 }}>
-      <Text style={{ fontSize: 18, marginBottom: 10 }}>Calendário de Eventos</Text>
-      
-      <Calendar
-        // Exibe o calendário
-        current={new Date().toISOString().split("T")[0]} // Data atual
-        markedDates={{
-          ...getMarkedDates(),
-          [new Date().toISOString().split("T")[0]]: { 
-            selected: true, 
-            selectedColor: "blue", 
-            selectedTextColor: "white",
-          },
-        }}
-        markingType={"simple"}
-      />
-    </View>
+    <ScrollView className="flex-1 px-6 bg-white">
+      <View>
+        <Calendar
+          current={today}
+          markingType={"custom"} 
+          markedDates={getMarkedDates()} 
+        />
+      </View>
+    </ScrollView>
   );
 };
 
