e30ef7351ae3512fb78ec58b17b4353b8dce83f1
VivianSTWu <vivian.stwu@gmail.com>
2025-05-04 18:12:10 -0300

    Correção router para voltar sempre para o appOdonto da pág calendario

--- MeuCalendario/app/_layout.tsx
+++ MeuCalendario/app/_layout.tsx
@@ -2,28 +2,37 @@ import { View, Text, TouchableOpacity } from 'react-native';
 import React from 'react';
 import "../global.css";
 import { StatusBar } from 'expo-status-bar';
-import { Slot, useRouter, useSegments } from 'expo-router';
+import { Slot, useRouter, useSegments, usePathname } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { ChevronLeft } from 'lucide-react-native';
 
 const RootLayout = () => {
     const router = useRouter();
     const segments = useSegments();
+    const pathname = usePathname();
 
-    // Oculta TODO o header em login e appOdonto
-    const ocultarHeader = ['login', 'appOdonto'].includes(segments[0] || '');
+    const isHiddenHeader = pathname === "/login" || pathname === "/appOdonto";
+    const isCalendario = pathname === "/calendario";
 
     return (
         <SafeAreaView className='h-full bg-white'>
-            {!ocultarHeader && (
+            {!isHiddenHeader && (
                 <View className='h-28 pb-3 mb-5 flex bg-blue-700 justify-end items-center relative'>
-                    <TouchableOpacity className="absolute left-4 bottom-3" onPress={() => router.back()}>
+                    <TouchableOpacity
+                        className="absolute left-4 bottom-3"
+                        onPress={() => {
+                            if (isCalendario) {
+                                router.replace("/appOdonto");
+                            } else {
+                                router.back();
+                            }
+                        }}
+                    >
                         <ChevronLeft size={24} color="white" />
                     </TouchableOpacity>
                     <Text className="text-white text-2xl">Meu Calendário</Text>
                 </View>
             )}
-
             <Slot />
         </SafeAreaView>
     );
